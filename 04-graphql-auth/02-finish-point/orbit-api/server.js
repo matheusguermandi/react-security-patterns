ad5d71652781e854f32937c15b8d1251067eba65
matheusguermandi <matheus_guermandi@hotmail.com>
2023-11-11 22:17:36 +0000

    (graphql-auth) chore:  add orbit server graphql AuthDirective implementation

--- 04-graphql-auth/02-finish-point/orbit-api/server.js
+++ 04-graphql-auth/02-finish-point/orbit-api/server.js
@@ -349,3 +349,54 @@ const typeDefs = gql`
       @auth(requires: [USER, ADMIN])
   }
 `;
+
+class AuthDirective extends SchemaDirectiveVisitor {
+  visitObject(type) {
+    this.ensureFieldsWrapped(type);
+    type._requiredAuthRole = this.args.requires;
+  }
+
+  visitFieldDefinition(field, details) {
+    this.ensureFieldsWrapped(details.objectType);
+    field._requiredAuthRole = this.args.requires;
+  }
+
+  ensureFieldsWrapped(objectType) {
+    if (objectType._authFieldsWrapped) return;
+    objectType._authFieldsWrapped = true;
+
+    const fields = objectType.getFields();
+
+    Object.keys(fields).forEach((fieldName) => {
+      const field = fields[fieldName];
+      const { resolve = defaultFieldResolver } = field;
+
+      field.resolve = async function (...args) {
+        const allowableRoles =
+          field._requiredAuthRole || objectType._requiredAuthRole;
+
+        if (!allowableRoles || !allowableRoles.length) {
+          return resolve.apply(this, args);
+        }
+
+        const context = args[2];
+        const { user } = context;
+
+        if (!user) {
+          throw new AuthenticationError("Not authorized");
+        }
+
+        if (
+          !checkUserRole(
+            user,
+            allowableRoles.map((role) => role.toLowerCase())
+          )
+        ) {
+          throw new AuthenticationError("Not authorized");
+        }
+
+        return resolve.apply(this, args);
+      };
+    });
+  }
+}
