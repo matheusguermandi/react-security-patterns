7db8221b802e8e5bc6fe700de9525fd39b360fcd
matheusguermandi <matheus_guermandi@hotmail.com>
2023-11-11 22:15:20 +0000

    (graphql-auth) chore:  update orbit server graphql type definition

---
+++ 04-graphql-auth/02-finish-point/orbit-api/server.js
@@ -0,0 +1,120 @@
+require("dotenv").config();
+const jwtDecode = require("jwt-decode");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const dashboardData = require("./data/dashboard");
+const User = require("./data/User");
+const InventoryItem = require("./data/InventoryItem");
+
+const {
+  ApolloServer,
+  gql,
+  ApolloError,
+  AuthenticationError,
+  UserInputError,
+} = require("apollo-server");
+const { SchemaDirectiveVisitor } = require("graphql-tools");
+const { defaultFieldResolver } = require("graphql");
+
+const { createToken, hashPassword, verifyPassword } = require("./util");
+
+const checkUserRole = (user, allowableRoles) => {
+  if (!user || !allowableRoles.includes(user.role)) {
+    throw new AuthenticationError("Not authorized");
+  }
+  return true;
+};
+
+const typeDefs = gql`
+  directive @auth(requires: [Role] = [ADMIN]) on OBJECT | FIELD_DEFINITION
+
+  enum Role {
+    ADMIN
+    USER
+  }
+
+  type Sale {
+    date: String!
+    amount: Int!
+  }
+
+  type DashboardData {
+    salesVolume: Int!
+    newCustomers: Int!
+    refunds: Int!
+    graphData: [Sale!]!
+  }
+
+  type User {
+    _id: ID!
+    firstName: String!
+    lastName: String!
+    email: String!
+    role: String!
+    avatar: String
+    bio: String
+  }
+
+  type InventoryItem {
+    _id: ID!
+    user: String!
+    name: String!
+    itemNumber: String!
+    unitPrice: String!
+    image: String!
+  }
+
+  type AuthenticationResult {
+    message: String!
+    userInfo: User!
+    token: String!
+    expiresAt: String!
+  }
+
+  type InventoryItemResult {
+    message: String!
+    inventoryItem: InventoryItem
+  }
+
+  type UserUpdateResult {
+    message: String!
+    user: User!
+  }
+
+  type UserBioUpdateResult {
+    message: String!
+    userBio: UserBio!
+  }
+
+  type UserBio {
+    bio: String!
+  }
+
+  type Query {
+    dashboardData: DashboardData @auth(requires: [USER, ADMIN])
+    users: [User] @auth(requires: ADMIN)
+    user: User @auth(requires: [USER, ADMIN])
+    inventoryItems: [InventoryItem] @auth(requires: ADMIN)
+    userBio: UserBio @auth(requires: [USER, ADMIN])
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): AuthenticationResult
+    signup(
+      firstName: String!
+      lastName: String!
+      email: String!
+      password: String!
+    ): AuthenticationResult
+    addInventoryItem(
+      name: String!
+      itemNumber: String!
+      unitPrice: Float!
+    ): InventoryItemResult @auth(requires: ADMIN)
+    deleteInventoryItem(id: ID!): InventoryItemResult @auth(requires: ADMIN)
+    updateUserRole(role: String!): UserUpdateResult
+      @auth(requires: [USER, ADMIN])
+    updateUserBio(bio: String!): UserBioUpdateResult
+      @auth(requires: [USER, ADMIN])
+  }
+`;
